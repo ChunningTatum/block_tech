b51bfd9a5ec8b29758c9534c41b51f98386b4427
Chun <45426792+ChunningTatum@users.noreply.github.com>
2019-02-05 14:29:43 +0100

    Update |1-looping-a-triangle.js

--- template-repo/|1-looping-a-triangle.js
+++ template-repo/|1-looping-a-triangle.js
@@ -1,3 +1,15 @@
+//Looping a triangle
+//Write a loop that makes seven calls to console.log to output the following triangle:
+
+//#
+//##
+//###
+//####
+//#####
+//######
+//#######
+//It may be useful to know that you can find the length of a string by writing .length after it.
+
 let tag = "";
 while (tag.length <= 6) {
   tag = tag += "#";

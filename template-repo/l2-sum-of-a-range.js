7d651b09cb2aca0884ba27d1c5514a54c5423eaf
Chun <45426792+ChunningTatum@users.noreply.github.com>
2019-02-08 10:24:33 +0100

    Update l2-sum-of-a-range.js

--- template-repo/l2-sum-of-a-range.js
+++ template-repo/l2-sum-of-a-range.js
@@ -12,18 +12,27 @@
 //Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 
 
-const isEven = function(x) {
-	if(x % 2 == 0) {
-		return "True";
-     } else{
-		return "False";
-     }
+// Your code here.
+function range(start, end){
+  let tableOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return tableOfNumbers;
+}
+
+function sum(array){
+  let tableOfNumbers2 = [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let totaal = 0;
+  
+  for(i = 0; i < tableOfNumbers2.length; i++){
+    totaal += i;
+  }
+  
+  return totaal;
 }
 
 
-console.log(isEven(50));
-// → true
-console.log(isEven(75));
-// → false
-console.log(isEven(-1));
-// → ??
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55

969fd4a84e20055b37cfcee66658423f5f3e39f5
Chun <45426792+ChunningTatum@users.noreply.github.com>
2019-02-14 12:54:46 +0100

    Update l3-flattening.js

--- template-repo/l3-flattening.js
+++ template-repo/l3-flattening.js
@@ -1,1 +1,15 @@
- 
+//Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+
+
+//Array die ik om moet zetten naar → [1, 2, 3, 4, 5, 6]
+let arrays = [[1, 2, 3], [4, 5], [6]];
+//Maak een lege array aan, waar de oude array in moet
+let newArray = [];
+
+//For loop, omdat het een mulidimentionaal array is en geconvert moet worden naar een single dimentionaal.
+for (var i = 0; i < arrays.length; i++) {
+  newArray = newArray.concat(arrays[i]);
+}
+
+console.log(newArray);
+// → [1, 2, 3, 4, 5, 6]
